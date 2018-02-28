f4eca67fe51b1e361f6af076f226af783dc30d52
Deasun OD <des_donn@mailbox.org>
2018-02-28 19:06:58 +0000

    Routing between four main pages set up

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World"
+def index():
+    return render_template("index.html")
 
+@app.route('/challenge')
+def challenge():
+    return render_template("challenge.html")
+    
+@app.route('/information')
+def information():
+    return render_template("information.html")
+    
+@app.route('/message_board')
+def message_board():
+    return render_template("message_board.html")
+    
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+        port=int(os.environ.get('PORT')),
+        debug=True)
