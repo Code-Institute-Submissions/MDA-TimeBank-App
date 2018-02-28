fb4d4e576343b0e06076c4ff6f44ab57a14e673b
Deasun OD <des_donn@mailbox.org>
2018-02-28 03:12:15 +0000

    Questions_Answers function: Test to ensure 'close enough' answer accepted and 5 points awarded

--- guess_game.py
+++ guess_game.py
@@ -23,11 +23,13 @@ def validate_email_address(email):
 QUESTIONS_ANSWERS FUNCTION
 """
 def ask_questions(guess, answer):
-    if guess == answer:
+    if guess is str(guess):
+        return 0
+    elif int(guess) == int(answer):
         return 10
-    elif guess > answer and guess < (answer + 10):
+    elif ((guess > answer) and (guess < (answer + 10))) or ((guess < answer) and (guess > (answer - 10))):
         return 5
-    elif guess < answer and guess > (answer - 10):
-        return 5
+    else:
+        return 0
    
    
--- test_guess_game.py
+++ test_guess_game.py
@@ -52,13 +52,31 @@ class test_guess_game(unittest.TestCase):
     
     def test_ask_questions(self):
         """
-        Test to check whether answer is correct 
+        Test to check whether 10 awarded for correct answers 
         """
         self.assertEqual(guess_game.ask_questions(23, 23), 10)
         self.assertNotEqual(guess_game.ask_questions("hello", 23), 10)
         self.assertNotEqual(guess_game.ask_questions("&%$", 3), 10)
         self.assertNotEqual(guess_game.ask_questions(32, 3), 10)
       
+      
+    def test_questions_scores_5(self):
+        """
+        Test to check whether 5 awarded for answers which are within range of  
+        10 lower or higher than answer
+        """
+        self.assertEqual(guess_game.ask_questions(23, 30), 5)
+        self.assertEqual(guess_game.ask_questions(39, 30), 5)
+        self.assertNotEqual(guess_game.ask_questions("hello", 30), 5)    
+    
+    def test_questions_scores_0(self):
+        """
+        Test to check whether 5 awarded for answers which are within range of  
+        10 lower or higher than answer
+        """
+        self.assertEqual(guess_game.ask_questions(19, 30), 0)
+        self.assertEqual(guess_game.ask_questions(41, 30), 0)
+        self.assertEqual(guess_game.ask_questions("hello", 30), 0)
 
 if __name__ == '__main__':
     unittest.main()
