107331f682f5b0d0e9079ccef133aec6ff55dcad
Deasun OD <des_donn@mailbox.org>
2018-03-01 04:19:32 +0000

    Images uploaded for Challenge, index.html formatted and sample quiz page (challenge.html) created

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_title="Welcome")
 
 @app.route('/challenge')
 def challenge():
-    return render_template("challenge.html")
+    return render_template("challenge.html", page_title="Take the timeBank Challenge")
     
 @app.route('/information')
 def information():
-    return render_template("information.html")
+    return render_template("information.html", page_title="Find out more...")
     
 @app.route('/message_board')
 def message_board():
-    return render_template("message_board.html")
+    return render_template("message_board.html", page_title="What do you think? Leave a message")
     
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
