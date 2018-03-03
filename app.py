893bc400f26a4cb7682c945723700c74acddb10d
Deasun OD <des_donn@mailbox.org>
2018-03-03 05:20:22 +0000

    Flash imported into flask & Username Display on Challenge.html added

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+
 
 app = Flask(__name__)
+app.secret_key = "some_secret"
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        print(request.form)
+        flash("Welcome {}!".format(
+            request.form["username"]
+        ))
     return render_template("index.html")
 
 @app.route('/challenge')
