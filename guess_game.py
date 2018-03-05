21bea1aa2c6ea776fab76e3368ade6546fbbc62c
Deasun OD <des_donn@mailbox.org>
2018-03-05 18:54:00 +0000

    Unittest on function (limit_number_questions) to limit number of Guesses to Two per Challenge and revise scoring matrix

--- guess_game.py
+++ guess_game.py
@@ -2,21 +2,24 @@
 REGISTER FUNCTION - Validated but simplified with bootstrap
 """
 """
-QUESTIONS_ANSWERS FUNCTION
-# """
-def ask_questions(guess, answer):
-    score = 0
-    if guess is str(guess):
-        score = score + 0
-    elif guess == answer:
-        score = score + 10
-    elif ((guess > answer) and (guess < (answer + 10))) or ((guess < answer) and (guess > (answer - 10))):
-        score = score + 5
-    else:
-        score = score + 0
-    return score
+QUESTIONS_ANSWERS FUNCTION - Validated and defined& called on view page
+"""
 
-  
+
+"""
+LIMIT # OF QUESTIONS TO 2
+"""
+def limit_number_questions(guess_1, guess_2, answer):
+    if guess_1 == answer:
+        return 10
+    else:
+        if guess_2 == answer:
+            return 8
+        elif ((guess_2 > answer) and (guess_2 < (answer + 10))) or ((guess_2 < answer) and (guess_2 > (answer - 10))):
+            return 5
+        else:
+            return 0
+        
 
 
    
--- test_guess_game.py
+++ test_guess_game.py
@@ -7,35 +7,47 @@ class test_guess_game(unittest.TestCase):
     """
     
     """
-    Quetions_Answers Function
+    Max 2 guesses test_guess_game
     """
-    
-    def test_ask_questions(self):
+    def test_limit_number_questions(self):
         """
-        Test to check whether 10 awarded for correct answers 
+        Test to check can take three arguments (2 guesses, 1 answer), exit if
+        guess correct on first attempt, and return 10 points
         """
-        self.assertEqual(guess_game.ask_questions(23, 23), 10)
-        self.assertNotEqual(guess_game.ask_questions("hello", 23), 10)
-        self.assertNotEqual(guess_game.ask_questions("&%$", 3), 10)
-        self.assertNotEqual(guess_game.ask_questions(32, 3), 10)
-      
-    def test_questions_scores_5(self):
+        self.assertNotEqual(guess_game.limit_number_questions(32, 21, 59), 10)
+        self.assertNotEqual(guess_game.limit_number_questions("Mum", 21, 59), 10)
+        self.assertEqual(guess_game.limit_number_questions(59, 21, 59), 10)
+        self.assertNotEqual(guess_game.limit_number_questions(21, 59, 59), 10)
+       
         """
-        Test to check whether 5 awarded for answers which are within range of  
-        10 lower or higher than answer
+        Test to check can take three arguments (2 guesses, 1 answer), move to
+        second correct guess if first incorrect, and return 8 points
         """
-        self.assertEqual(guess_game.ask_questions(23, 30), 5)
-        self.assertEqual(guess_game.ask_questions(39, 30), 5)
-        self.assertNotEqual(guess_game.ask_questions("hello", 30), 5)    
-    
-    def test_questions_scores_0(self):
+        self.assertNotEqual(guess_game.limit_number_questions(59, 32, 59), 8)
+        self.assertNotEqual(guess_game.limit_number_questions(59, "Crazy", 59), 8)
+        self.assertEqual(guess_game.limit_number_questions(32, 59, 59), 8)
+        self.assertNotEqual(guess_game.limit_number_questions(59, 59, 59), 8)
+        
         """
-        Test to check whether 0 awarded for incorrect and invalid answers
+        Test to check if second guess within <10 or >10 returns 5 points
         """
-        self.assertEqual(guess_game.ask_questions(19, 30), 0)
-        self.assertEqual(guess_game.ask_questions(41, 30), 0)
-        self.assertEqual(guess_game.ask_questions("hello", 30), 0)
-        self.assertEqual(guess_game.ask_questions(-1, 30), 0)
+        self.assertNotEqual(guess_game.limit_number_questions(59, 55, 59), 5)
+        self.assertNotEqual(guess_game.limit_number_questions(6, 59, 59), 5)
+        self.assertEqual(guess_game.limit_number_questions(6, 55, 59), 5)
+        self.assertEqual(guess_game.limit_number_questions(6, 62, 59), 5)
+        self.assertNotEqual(guess_game.limit_number_questions("%&", 70, 59), 5)
+        
+        """
+        Test to check if second wrong guess returns 0 points
+        """
+        self.assertNotEqual(guess_game.limit_number_questions(3, 59, 59), 0)
+        self.assertNotEqual(guess_game.limit_number_questions(59, 3, 59), 0)
+        self.assertEqual(guess_game.limit_number_questions(3, 3, 59), 0)
+        self.assertEqual(guess_game.limit_number_questions("Crazy", 3, 59), 0)
+
+        
+
+        
 
 if __name__ == '__main__':
     unittest.main()
