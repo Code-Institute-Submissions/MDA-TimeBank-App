1ac695b07a2e4daf81157fc572f2c75aea54e60b
Deasun OD <des_donn@mailbox.org>
2018-03-12 15:58:57 +0000

    challenge page jquery transitions timed

--- static/js/challenge.js
+++ static/js/challenge.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
     // Numbers (need) on Challenge Pages dissolve in on page load
     $('body').click(function() {
         $('h1.hidden').fadeIn(1000).removeClass('hidden');
-        $('h2.hidden').show(1000).removeClass('hidden');
-        $('h1.hidden_title').delay(1000).show("fast").removeClass('hidden');
-        $('h2.hidden_title').delay(1000).show("fast").removeClass('hidden');
-        $('form.hidden_title').delay(1000).show("fast").removeClass('hidden');
+        $('h2.hidden').show(2000).removeClass('hidden');
+        $('h1.hidden_title').delay(3000).show("fast").removeClass('hidden');
+        $('h2.hidden_title').delay(5000).show("slow").removeClass('hidden');
+        $('form.hidden_title').delay(6500).show("fast").removeClass('hidden');
     });
 
     // Send Ajax request for guesses
@@ -35,9 +35,9 @@ $(document).ready(function() {
     // Transitions after button click
     $('.challenge_btn').click(function() {
         $(this).slideUp("fast");
-        $('h2.hidden_answer').fadeIn(4000).removeClass('hidden');
-        $('h1.hidden_answer').fadeIn(4000).removeClass('hidden');
-        $('.hidden_icon').delay(100).fadeIn(400).fadeOut(400).fadeIn(400).fadeOut(400).fadeIn(400).removeClass('hidden');
+        $('h2.hidden_answer').delay(1000).show("fast").removeClass('hidden');
+        $('h1.hidden_answer').delay(2000).show("fast").removeClass('hidden');
+        $('.hidden_icon').delay(4000).fadeIn(400).fadeOut(400).fadeIn(400).fadeOut(400).fadeIn(400).removeClass('hidden');
     });
 
 
