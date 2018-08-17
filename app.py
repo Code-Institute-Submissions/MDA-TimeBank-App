12579dbd4801c09ca757282033439dfaf8998873
Deasun OD <des_donn@mailbox.org>
2018-08-17 23:01:00 +0000

    removed debug mode for deployment

--- app.py
+++ app.py
@@ -214,8 +214,7 @@ def results_board():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+        port=int(os.environ.get('PORT')))
         
         
 
