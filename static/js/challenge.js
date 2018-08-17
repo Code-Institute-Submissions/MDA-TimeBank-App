939658624e5f8e95314d0e1fa01a2d4dec6d6f98
Deasun OD <des_donn@mailbox.org>
2018-08-17 18:39:35 +0000

    removed fadeIn/fadeOut on flash message and replaced with click to dismiss

--- static/js/challenge.js
+++ static/js/challenge.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $('.flash').animate({left: "15%"}, "slow", "swing").delay(6000).fadeOut('fast').delay(4000).show('fast');
+    $('.flash').animate({left: "15%"}, "slow", "swing").delay(6000);
 	
 	
 // 	Challenge page Question, Attempt and Score counters
@@ -16,8 +16,8 @@ $(document).ready(function() {
     $('.form-inline').delay(10000).fadeIn("slow");
 
 // Click to hide flash messages
-    $(".warning").click(function(){
-        $(".warning").hide();
+    $(".flash").click(function(){
+        $(this).hide();
     });
 // Hover effect on count cards
     $('.card').mouseover(function(){
@@ -26,34 +26,6 @@ $(document).ready(function() {
     $('.card').mouseout(function(){
         $(this).css('background-color', "rgba(66, 24, 59, 0.78)");
     });
+
     
-
-
-
-
-    // Numbers (need) on Challenge Pages dissolve in on page load
-    // $('body').click(function() {
-    //     $('h1.hidden').slideDown("slow").removeClass('hidden');
-    //     $('h2.hidden').slideDown("slow").removeClass('hidden');
-    //     $('h1.hidden_title').slideDown("slow").removeClass('hidden');
-    //     $('h2.hidden_title').slideDown("slow").removeClass('hidden');
-    //     $('form.hidden_title').slideDown("slow").removeClass('hidden');
-    // });
-
-
-    // Transitions after button click
-    // $('.challenge_btn').click(function() {
-    //     $(this).slideUp("fast");
-    //     $('h2.hidden_answer').delay(1000).show("fast").removeClass('hidden');
-    //     $('h1.hidden_answer').delay(2000).show("fast").removeClass('hidden');
-    //     $('.hidden_icon').delay(4000).fadeIn(400).fadeOut(400).fadeIn(400).fadeOut(400).fadeIn(400).removeClass('hidden');
-    // });
-    
-    // Display Score
-    // $('.username').slideDown(3000).removeClass('hidden');
-
-    // Page Alerts removed
-    // $("body").click(function() {
-    //     $(".alert").hide("fast");
-    // });
 });
